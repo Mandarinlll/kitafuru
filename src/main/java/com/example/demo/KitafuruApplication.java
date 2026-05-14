7554fde684729e62293526c3ba6be29900a080ea
SHOTA <shouta2004_i@icloud.com>
2026-05-14 15:28:17 +0900

    initialized

---
+++ src/main/java/com/example/demo/KitafuruApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitafuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitafuruApplication.class, args);
+	}
+
+}
