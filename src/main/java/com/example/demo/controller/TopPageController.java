dee77f43bdf55d2aba9c8609bb9c7ad11312a464
SHOTA <shouta2004_i@icloud.com>
2026-05-15 22:41:10 +0900

    季節おすすめ機能作業途中

--- src/main/java/com/example/demo/controller/TopPageController.java
+++ src/main/java/com/example/demo/controller/TopPageController.java
@@ -1,12 +1,23 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.service.ProductSeasonService;
 
 @Controller
 public class TopPageController {
+
+	private final ProductSeasonService productSeasonService;
+
+	public TopPageController(ProductSeasonService productSeasonService, RegisterController registerController) {
+		this.productSeasonService = productSeasonService;
+	}
+
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("seasonProducts", productSeasonService.getSeasonProducts());
 		return "home";
 	}
 }
---
+++ src/main/java/com/example/demo/mapper/ProductMapper.java
@@ -0,0 +1,12 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.entity.Product;
+
+@Mapper
+public interface ProductMapper {
+	List<Product> findAllSeason(int month);
+}
