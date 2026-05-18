50f704b23fd25d45d135c7143c4643dee29b285f
JP <maq931218@gmail.com>
2026-05-18 10:34:36 +0900

    生産者ダッシュボードとユーザ管理機能の追加

---
+++ src/main/java/com/example/demo/model/ProducerProfile.java
@@ -0,0 +1,94 @@
+package com.example.demo.model;
+
+public class ProducerProfile {
+	private int id;
+	private String shopName;
+	private String companyName;
+	private String representativeName;
+	private String area;
+	private String postalAddress;
+	private String phone;
+	private String email;
+	private String brandDescription;
+	private String snsLink;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getShopName() {
+		return shopName;
+	}
+
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getRepresentativeName() {
+		return representativeName;
+	}
+
+	public void setRepresentativeName(String representativeName) {
+		this.representativeName = representativeName;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getPostalAddress() {
+		return postalAddress;
+	}
+
+	public void setPostalAddress(String postalAddress) {
+		this.postalAddress = postalAddress;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getBrandDescription() {
+		return brandDescription;
+	}
+
+	public void setBrandDescription(String brandDescription) {
+		this.brandDescription = brandDescription;
+	}
+
+	public String getSnsLink() {
+		return snsLink;
+	}
+
+	public void setSnsLink(String snsLink) {
+		this.snsLink = snsLink;
+	}
+}
