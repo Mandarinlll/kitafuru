2a1cce365e6f42778ce8cd2a068cb7a62babd83c
SHOTA <shouta2004_i@icloud.com>
2026-05-19 14:43:45 +0900

    UI修正とSQL文にバグがあったため修正しました

--- src/main/java/com/example/demo/controller/AIRecommendController.java
+++ src/main/java/com/example/demo/controller/AIRecommendController.java
@@ -2,11 +2,14 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.entity.Product;
+import com.example.demo.entity.User;
 import com.example.demo.service.AIRecommendService;
 
 @Controller
@@ -19,10 +22,18 @@ public class AIRecommendController {
 	}
 
 	@GetMapping("/ai-recommend")
-	public String aiRecommend(Model model) {
-		//仮のユーザID
-		Integer userId = 1;
+	public String aiRecommend(HttpSession session, Model model) {
+		//sessionからログインユーザ取得
+		User loginUser = (User) session.getAttribute("loginUser");
+		//未ログイン対策
+		if (loginUser == null) {
+			return "redirect:/login";
+		}
 
+		//ログインユーザのIDを取得
+		Integer userId = loginUser.getId();
+
+		//おすすめ商品取得
 		List<Product> recommendedProducts = aiRecommendService.findRecommendProducts(userId);
 		model.addAttribute("recommendedProducts", recommendedProducts);
 		return "ai-recommend";
