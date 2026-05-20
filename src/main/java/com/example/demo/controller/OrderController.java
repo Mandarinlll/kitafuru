65d8c7c330fe724447e484a4bd200ed2b9cc837b
Masaki <mskamk4@gmail.com>
2026-05-20 10:55:04 +0900

    注文確定ボタンを実装

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -11,7 +11,12 @@ public class OrderController {
 	public String confirmOrder() {
 
 		return "order-confirmation";
-
 	}
 
+	// 注文完了画面表示
+	@GetMapping("/order-complete")
+	public String orderComplete() {
+
+		return "order-complete";
+	}
 }
